575038e1bb2f81ea8cb8e529d80e328b4e90becf
Manoj <mk8207245@gmail.com>
2024-10-16 11:29:11 +0530

    test

--- src/components/WAButton/Wabutton.jsx
+++ src/components/WAButton/Wabutton.jsx
@@ -5,7 +5,7 @@ const Wabutton = () => {
   return (
     <div>
         <a href="https://wa.me/918384085829?text=Hello%20I%20would%20like%20to%20inquire%20about%20your%20services" target="_blank">
-            <button className='wa-btn'>Chat With Us</button>
+            <button className='wa-btn'>Chat on Whatsapp</button>
         </a>
     </div>
   )
